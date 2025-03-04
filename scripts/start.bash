d900c87907af19e07e274610cac2ec5e9015ca41
bswck <bartosz@slawecki.dev>
2025-03-04 16:36:16 +0100

    Use `:` for bash logging

--- scripts/start.bash
+++ scripts/start.bash
@@ -1,7 +1,8 @@
 #!/usr/bin/env /bin/bash
 set -Eeuxo pipefail
 
-: \
+: Running migrations \
 && alembic upgrade head \
+&& : Running the consumer \
 && python -m consumer "${1:-consume}" \
-&& :
+&& : Finished
